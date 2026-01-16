82323033a73766d9b4da462394f7a35251d673c1
Azatgal <azat_gal@icloud.com>
2026-01-16 20:07:23 +0500

    fix

--- configs/pretrain_cfg.py
+++ configs/pretrain_cfg.py
@@ -15,8 +15,8 @@ cfg.exp_dir = os.path.join(ROOT_DIR, 'runs', 'pretrain',
 cfg.batch_size = 8 * 1024
 cfg.num_epoch = 125
 
-cfg.wu_ratio = 0.1
-cfg.decay_ratio = 0.75
+cfg.wu_ratio = 0.05
+cfg.decay_ratio = 0.5
 cfg.lr = 6e-4 * (cfg.batch_size / 256) ** 0.5  # 1e-3 bs: 8 * 1024
 cfg.lr_decay_factor = 1e-2
 cfg.lr_decay = 'cosine'
