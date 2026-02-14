1fabe2d238c1c7054a9953d0c2b3f6243b0f8e86
Azatgal <azat_gal@icloud.com>
2026-02-14 23:14:46 +0400

    bugfix

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -6,8 +6,8 @@ cfg = EasyDict()
 
 cfg.num_q_heads = 16  # 16
 cfg.num_kv_heads = 4  # 8
-cfg.embed_dim = 10 * cfg.num_q_heads  # 8 10 * cfg.num_q_heads
-cfg.num_blocks = 36  # 10  # 40
+cfg.embed_dim = 8 * cfg.num_q_heads  # 8 10 * cfg.num_q_heads
+cfg.num_blocks = 24  # 10  # 40
 cfg.act = 'ReLU'  # SiLU
 
 cfg.pool = 'avg'  # avg cls sum
