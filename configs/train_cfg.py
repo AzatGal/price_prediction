5b9a15521baf2ea216f5218f8ec3ec438d199ea0
Azatgal <azat_gal@icloud.com>
2026-02-14 21:09:36 +0400

    bugfix

--- configs/train_cfg.py
+++ configs/train_cfg.py
@@ -13,7 +13,7 @@ cfg = EasyDict()
 cfg.seed = 0
 cfg.exp_dir = os.path.join(ROOT_DIR, 'runs', 'train',
                            datetime.now().strftime("%d-%m_%H-%M"))
-cfg.batch_size = 2048  # 1024
+cfg.batch_size = 1024
 cfg.num_epoch = 400
 
 cfg.wu_ratio = 0.05  # 0.1
