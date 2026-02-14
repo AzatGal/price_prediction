6bd0cc402e0845b223bfcea941d1e35f9b6e3273
Azatgal <azat_gal@icloud.com>
2026-02-14 21:26:05 +0400

    bugfix

--- analysis/features_analysis.py
+++ analysis/features_analysis.py
@@ -43,38 +43,23 @@ def feature_analysis():
         print(col, '\tНепрерывный\t', len(edges))
 
 
-@torch.no_grad()
 def main():
-    # path = '/Users/azatgalautdinov/Desktop/price_prediction'
-    # with open(os.path.join(path, 'logs', 'config.json'), 'r') as f:
-    #     model_cfg = json.load(f)['model_cfg']
-    #
-    # cfg.model_cfg = model_cfg
-    # trainer = Trainer(cfg, False)
-    # trainer.load_model(os.path.join(path, 'PricePrediction.pt'))
-    # embed = trainer.model.embed
-    #
-    # def kv_compressor_weights(compressor):
-    #     if isinstance(compressor, nn.ModuleList):
-    #         return sum([x.weight.abs().sum(dim=0).cpu().numpy() for x in compressor])
-    #     elif isinstance(compressor, nn.Linear):
-    #         return compressor.weight.abs().sum(dim=0).cpu().numpy()
-    #     raise NotImplementedError
-    #
-    #
-    #
-    # ids = np.argsort(w)[::-1]
-    # features = (cfg.data_cfg.data_transformer.num_cols +
-    #             cfg.data_cfg.data_transformer.cat_cols)
-    # print(w[ids])
-    # print(np.array(features[0 if cfg.data_cfg.include_target else 1:])[ids])
+    path = '/Users/azatgalautdinov/Desktop/price_prediction/best'
+    with open(os.path.join(path, 'logs', 'config.json'), 'r') as f:
+        model_cfg = json.load(f)['model_cfg']
 
-    cfg.load_checkpoint = '/Users/azatgalautdinov/Desktop/price_prediction/best/checkpoint'
-    trainer = Trainer(cfg)
-    print(trainer.optimizer.param_groups[0]['lr'])
+    cfg.model_cfg = model_cfg
+    trainer = Trainer(cfg, False)
+    trainer.load_model(os.path.join(path, 'TablePredictor.pt'))
+    model = trainer.model
+
+    print(model.kv_compressors[0][1].weight)
+    # cfg.load_checkpoint = '/Users/azatgalautdinov/Desktop/price_prediction/best/checkpoint'
+    # trainer = Trainer(cfg)
+    # print(trainer.optimizer.param_groups[0]['lr'])
 
 
 if __name__ == '__main__':
-    # main()
+    main()
     # data_analysis()
-    feature_analysis()
+    # feature_analysis()
--- configs/train_cfg.py
+++ configs/train_cfg.py
@@ -29,7 +29,7 @@ cfg.weight_decay = 0.1  # 3e-4
 cfg.loss = 'L1Loss'  # SmoothL1Loss L1Loss MSELoss LogCosh HuberLoss
 cfg.loss_args = {}  # 'delta': 8}  # 'reduction': 'batchmean'}
 
-cfg.accelerator_args = {'mixed_precision': 'fp16'}  # , 'cpu': True}
+cfg.accelerator_args = {}  # 'mixed_precision': 'fp16'}  # , 'cpu': True}
 
 # cfg.load_checkpoint = os.path.join(ROOT_DIR, 'runs', 'train', '14-02_13-12', "checkpoint")
 
