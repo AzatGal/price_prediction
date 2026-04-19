5e1038b2a5b33037d8dce94f03843b43808d6b47
Azatgal <azat_gal@icloud.com>
2026-04-19 20:18:40 +0300

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -4,10 +4,10 @@ from configs.data_cfg import cfg as data_cfg
 
 cfg = EasyDict()
 
-cfg.num_q_heads = 4
-cfg.num_kv_heads = 2  # 4
-cfg.embed_dim = 6 * cfg.num_q_heads  # 8 10 * cfg.num_q_heads
-cfg.num_blocks = 120  # 10 24 40
+cfg.num_q_heads = 16
+cfg.num_kv_heads = 8  # 4
+cfg.embed_dim = 10 * cfg.num_q_heads  # 8 10 * cfg.num_q_heads
+cfg.num_blocks = 40  # 10 24 40
 cfg.act = 'ReLU'  # SiLU
 
 cfg.pool = 'avg'  # avg cls sum
@@ -18,11 +18,11 @@ cfg.num_embed_features = (
     data_cfg.data_transformer.num_cats
 )
 
-cfg.attn_dropout = 0.0
+cfg.attn_dropout = 0.1
 cfg.mlp_dropout = 0.1
 cfg.dropout = 0.1
 cfg.kv_compression = 'Head'  # Head KV Layer
-cfg.kv_compression_dim = 4  # 0.01
+cfg.kv_compression_dim = 1  # 0.01
 cfg.mlp_dim_factor = 2  # 2 8 / 3
 
 cfg.attn = 'Attention'  # Linear
