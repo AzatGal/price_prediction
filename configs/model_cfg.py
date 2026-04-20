0bf01f564cbd8adb1dbc4f5ab7802333152a5c6d
Azatgal <azat_gal@icloud.com>
2026-04-20 21:53:58 +0300

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -7,7 +7,7 @@ cfg = EasyDict()
 cfg.num_q_heads = 4
 cfg.num_kv_heads = 2  # 4
 cfg.embed_dim = 4 * cfg.num_q_heads  # 8 10 * cfg.num_q_heads
-cfg.num_blocks = 120  # 10 24 40
+cfg.num_blocks = 160  # 10 24 40
 cfg.act = 'ReLU'  # SiLU
 
 cfg.pool = 'avg'  # avg cls sum
