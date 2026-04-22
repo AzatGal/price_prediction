5746f2eb57c4de8d141570e40776206be04078a9
Azatgal <azat_gal@icloud.com>
2026-04-22 12:46:17 +0300

    experiment

--- models/transformers.py
+++ models/transformers.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 from models.modules.embedding import FeatureEmbedding
 from models.modules.block import TransformerBlock, CompressorBlock
-from models.modules.mlp import GatedMLP
+# from models.modules.mlp import GatedMLP
 
 
 class Transformer(nn.Module):
@@ -74,7 +74,7 @@ class Transformer(nn.Module):
     def reset_parameters(self) -> None:
         for pn, p in self.named_parameters():
             if 'norm' not in pn:
-                if 'bias' in pn:  # or 'compressors' in pn:  # or 'qkv' in pn or 'out_proj' in pn:
+                if 'bias' in pn or 'compressor' in pn:  # or 'qkv' in pn or 'out_proj' in pn:
                     nn.init.zeros_(p)
                 elif 'head' in pn:
                     nn.init.kaiming_uniform_(p, a=math.sqrt(5))
