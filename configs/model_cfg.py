a15c3ee0350ac8d9d4c71164737064e44bab079d
Azatgal <azat_gal@icloud.com>
2026-04-28 12:37:57 +0300

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -11,7 +11,7 @@ cfg.embed_dim = 32  # 8 10 * cfg.num_q_heads
 cfg.num_blocks = 3  # 10 24 40
 cfg.act = 'ReLU'  # SiLU
 
-cfg.pool = 'w_avg'  # avg cls sum w_avg
+cfg.pool = 'cls'  # avg cls sum w_avg
 cfg.mask_first_token = False  # True False
 cfg.add_cls_token = cfg.pool == 'cls' and not cfg.mask_first_token
 cfg.num_embed_features = (
