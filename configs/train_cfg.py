43c9d4a942aa962154e5dedd505f12b27bcb1db6
Azatgal <azat_gal@icloud.com>
2026-04-29 14:23:56 +0300

    experiment

--- configs/train_cfg.py
+++ configs/train_cfg.py
@@ -13,7 +13,7 @@ cfg = EasyDict()
 cfg.seed = 0
 cfg.exp_dir = os.path.join(ROOT_DIR, 'runs',
                            datetime.now().strftime("%d-%m_%H-%M"))
-cfg.batch_size = 64  # 1024  # 2048 1024
+cfg.batch_size = 1024  # 2048 1024
 cfg.num_epoch = 600
 
 cfg.wu_ratio = 0.05  # 0.1
