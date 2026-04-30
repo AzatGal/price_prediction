a3f8f969db75b1e5f334a87a0c25d41a07744a93
Azatgal <azat_gal@icloud.com>
2026-04-30 10:37:44 +0300

    experiment

--- configs/data_cfg.py
+++ configs/data_cfg.py
@@ -63,7 +63,7 @@ processors = EasyDict(
         # FunctionTransformer(np.nan_to_num),
         # FunctionTransformer(lambda x: x.astype(np.float32)),
         FunctionTransformer(lambda x: x.fillna(raw_data.train[columns.num].min() - 100)), # nan - как отдельный эмбеддинг
-        KBinsDiscretizer(n_bins=128, encode='ordinal', strategy='kmeans'),
+        KBinsDiscretizer(n_bins=1000, encode='ordinal', strategy='kmeans'),
         FunctionTransformer(lambda x: x.astype(int))
     ),
     cat=make_pipeline(
