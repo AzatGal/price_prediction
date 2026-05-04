7fdb9e452cb6c5ade68b19441615c98a785a6e7c
Azatgal <azat_gal@icloud.com>
2026-05-04 18:43:26 +0300

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -5,8 +5,8 @@ from easydict import EasyDict
 cfg = EasyDict()
 
 # cfg.k = 2
-cfg.k = 8  # 16
-cfg.embed_dim = 32
+cfg.k = 4  # 16
+cfg.embed_dim = 64
 cfg.num_blocks = 3
 cfg.act = 'ReLU'  # SiLU
 cfg.pred_dim = 1
@@ -23,7 +23,7 @@ cfg.kv_compression_dim = 4
 cfg.mlp_dim_factor = 3 / 2  # 2 8 / 3
 
 cfg.attn = 'AttentionEnsemble'  # Linear
-cfg.mlp = 'GatedMLP'  # GatedMLP MLP
+cfg.mlp = 'MLP'  # GatedMLP MLP
 cfg.norm = 'RMSNorm'  # LayerNorm RMSNorm
 
 
