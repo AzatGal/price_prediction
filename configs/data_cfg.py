95550fa2cdecb26bab6f918e142c74a8c29588c4
Azatgal <azat_gal@icloud.com>
2026-05-04 19:55:57 +0300

    experiment

--- configs/data_cfg.py
+++ configs/data_cfg.py
@@ -76,7 +76,7 @@ warnings.filterwarnings('ignore', category=ConvergenceWarning, module='sklearn')
 cfg = EasyDict(
     raw_data=raw_data,
     n_num=len(columns.num),
-    n_cat=[len(c) for c in cats],
+    n_cat=[len(c) + 1 for c in cats],
     # columns=columns,
     processors=EasyDict(
         num=make_pipeline(
