b8749421f2eefa51f0f6b57c51e21698eb03260b
Azatgal <azat_gal@icloud.com>
2026-05-04 14:27:10 +0300

    experiment

--- models/transformers.py
+++ models/transformers.py
@@ -483,16 +483,15 @@ class TransformerEnsemble(nn.Module):
         self.reset_parameters()
 
     def _get_compressor(self) -> nn.Module:
-        return nn.Linear(
-            self.seq_len,
-            self.kv_compression_dim,
-        )
-        # return LinearEnsemble(
+        # return nn.Linear(
         #     self.seq_len,
         #     self.kv_compression_dim,
-        #     self.k,
-        #     False
         # )
+        return LinearEnsemble(
+            self.seq_len,
+            self.kv_compression_dim,
+            self.k
+        )
 
     def reset_parameters(self) -> None:
         for pn, p in self.named_parameters():
