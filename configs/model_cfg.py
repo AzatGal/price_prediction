386298d78af1fff4f6ec20ce701e015a738076be
Azatgal <azat_gal@icloud.com>
2026-05-08 12:15:35 +0400

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -6,9 +6,9 @@ cfg = EasyDict()
 
 # cfg.k = 2
 # cfg.embed_dim = 4
-cfg.k = 4  # 16
+cfg.k = 12  # 16
 cfg.embed_dim = 96  # 32
-cfg.num_blocks = 7  # 3
+cfg.num_blocks = 5  # 3
 cfg.act = 'ReLU'  # SiLU
 cfg.pred_dim = 1
 
@@ -20,7 +20,7 @@ cfg.mlp_dropout = 0.0
 cfg.dropout = 0.0
 
 cfg.kv_compression = 'Head'  # Head KV Layer  None
-cfg.kv_compression_ratio = 0.2
+cfg.kv_compression_ratio = 0.1
 cfg.mlp_dim_factor = 3 / 2  # 2 8 / 3
 
 cfg.attn = 'AttentionEnsemble'  # Linear
