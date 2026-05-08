762e801679018e8e587794ca2b3f238d8dc31ae2
Azatgal <azat_gal@icloud.com>
2026-05-08 12:00:00 +0400

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -6,9 +6,9 @@ cfg = EasyDict()
 
 # cfg.k = 2
 # cfg.embed_dim = 4
-cfg.k = 8  # 16
-cfg.embed_dim = 64  # 32
-cfg.num_blocks = 5  # 3
+cfg.k = 4  # 16
+cfg.embed_dim = 96  # 32
+cfg.num_blocks = 7  # 3
 cfg.act = 'ReLU'  # SiLU
 cfg.pred_dim = 1
 
