1f940300d404c9d1e394048fb0d52f817176238f
Azatgal <azat_gal@icloud.com>
2026-05-08 13:26:48 +0400

    experiment

--- models/modules/embedding.py
+++ models/modules/embedding.py
@@ -68,8 +68,8 @@ class FeatureTokenizerEnsemble(nn.Module):
         if isinstance(n_embed_num, int):
             self.n_num = n_embed_num
 
-            self.num_weight = nn.Parameter(torch.empty(k, self.n_num, 1, 2*embed_dim))
-            self.num_bias = nn.Parameter(torch.empty(k, self.n_num, 1, 2*embed_dim))
+            self.num_weight = nn.Parameter(torch.empty(k, self.n_num, 1, embed_dim))
+            self.num_bias = nn.Parameter(torch.empty(k, self.n_num, 1, embed_dim))
             self.num_act = getattr(nn, num_act)()
         else:
             self.n_num = 0
@@ -149,8 +149,8 @@ class FeatureTokenizerEnsemble(nn.Module):
                 .repeat(1, self.k, 1, 1, 1)
             )
             x_num = x_num @ self.num_weight + self.num_bias
-            x_num, x_num_gate = x_num.chunk(2, -1)
-            x_num = self.num_act(x_num) * x_num_gate
+            # x_num, x_num_gate = x_num.chunk(2, -1)
+            x_num = self.num_act(x_num)  # * x_num_gate
             x_num = x_num.squeeze(-2)
 
         assert torch.all(x_cat < self.n_embed_cat)
