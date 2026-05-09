a7946075320a50b46e7a3ef435b4e30b9243cdd6
Azatgal <azat_gal@icloud.com>
2026-05-09 20:38:22 +0400

    experiment

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -18,7 +18,7 @@ cfg.attn_dropout = 0.0  # 1
 cfg.mlp_dropout = 0.0
 cfg.dropout = 0.0
 
-cfg.kv_compression_ratio = 0.2
+cfg.kv_compression_ratio = 0.1
 cfg.mlp_dim_factor = 3 / 2  # 2 8 / 3
 
 cfg.attn_bias = True
