f5b57f73d72dbf7c9d8ea71570e73c44668e0185
Azatgal <azat_gal@icloud.com>
2026-05-14 14:50:13 +0400

    refactor(models): simplify transformer ensemble architecture and make it parameterized

--- configs/model_cfg.py
+++ configs/model_cfg.py
@@ -3,10 +3,10 @@ from easydict import EasyDict
 
 cfg = EasyDict()
 
-# cfg.k = 2
-# cfg.embed_dim = 4
-cfg.k = 8
-cfg.embed_dim = 32  # 128  # 32
+cfg.k = 2
+cfg.embed_dim = 4
+# cfg.k = 8
+# cfg.embed_dim = 32  # 128  # 32
 cfg.num_blocks = 3
 cfg.act = 'ReLU'  # SiLU
 cfg.pred_dim = 1  # 1
