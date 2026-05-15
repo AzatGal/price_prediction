1c734c10df55eb788469cbce7ad1caf2c5d64781
Azatgal <azat_gal@icloud.com>
2026-05-15 23:38:42 +0400

    fix(data): move noise augmentation to data preparation stage
    Remove add_noise parameter from CustomDataset and apply noise
    to raw training data before fitting the QuantileTransformer
    instead of adding it dynamically to targets during training.

--- models/modules/embedding.py
+++ models/modules/embedding.py
@@ -69,7 +69,7 @@ class FeatureTokenizerEnsemble(nn.Module):
         if isinstance(n_embed_num, int):
             self.n_num = n_embed_num
 
-            self.num_weight = nn.Parameter(torch.empty(k_, self.n_num, embed_dim))
+            self.num_weight = nn.Parameter(torch.empty(k_, self.n_num, 2*embed_dim))
             # self.num_bias = nn.Parameter(torch.empty(k_, self.n_num, embed_dim))
         else:
             self.n_num = 0
@@ -110,7 +110,8 @@ class FeatureTokenizerEnsemble(nn.Module):
             x_num = None
         else:
             x_num = x_num.reshape(-1, 1, self.n_num, 1)
-            x_num = F.relu(x_num * self.num_weight)
+            x_num_1, x_num_2 = (x_num * self.num_weight).chunk(2, dim=-1)
+            x_num = F.relu(x_num_1) * x_num_2
 
         if x_cat is None:
             x = x_num
